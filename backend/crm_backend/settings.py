06661f688426516de58147c6430521ec692ddc42
fida salam <fidasalamop.foxiom@gmail.com>
2026-01-28 18:39:49 +0530

    deployment

--- backend/crm_backend/settings.py
+++ backend/crm_backend/settings.py
@@ -1,24 +1,223 @@
+# import os
+# from pathlib import Path
+# from datetime import timedelta
+# import dj_database_url
+
+
+# # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-$n#o3xf@yjz*(8y1g+kw+h5yve6gip4cmp*0hm_lyo%!t7aul='
+
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+
+# ALLOWED_HOSTS = []
+
+
+# # Application definition
+
+# INSTALLED_APPS = [
+#    'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+    
+#     # Third party
+#     'rest_framework',
+#     'rest_framework_simplejwt',  # ADD THIS
+#     'corsheaders',
+    
+#     # Local apps
+#     'customers',
+# ]
+
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     'corsheaders.middleware.CorsMiddleware', 
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
+
+# ROOT_URLCONF = 'crm_backend.urls'
+
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+
+# WSGI_APPLICATION = 'crm_backend.wsgi.application'
+
+
+# # Database
+# # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+# # DATABASES = {
+# #     'default': {
+# #         'ENGINE': 'django.db.backends.postgresql',
+# #         'NAME': os.environ.get('POSTGRES_DB', 'crm_db'),
+# #         'USER': os.environ.get('POSTGRES_USER', 'crm_user'),
+# #         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'crm_password'),
+# #         'HOST': os.environ.get('DATABASE_HOST', 'db'),
+# #         'PORT': os.environ.get('DATABASE_PORT', '5432'),
+# #     }
+# # }
+
+# from decouple import config
+
+# # Database configuration
+# DATABASE_ENGINE = config('DATABASE_ENGINE', default='sqlite')
+
+# if DATABASE_ENGINE == 'postgresql':
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': config('POSTGRES_DB', default='crm_db'),
+#             'USER': config('POSTGRES_USER', default='crm_user'),
+#             'PASSWORD': config('POSTGRES_PASSWORD', default='crm_password'),
+#             'HOST': config('DATABASE_HOST', default='127.0.0.1'),
+#             'PORT': config('DATABASE_PORT', default='5432'),
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+
+# # Password validation
+# # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
+
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
+
+
+# # Internationalization
+# # https://docs.djangoproject.com/en/6.0/topics/i18n/
+
+# LANGUAGE_CODE = 'en-us'
+
+# TIME_ZONE = 'UTC'
+
+# USE_I18N = True
+
+# USE_TZ = True
+
+
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/6.0/howto/static-files/
+
+# STATIC_URL = 'static/'
+
+# # CORS Settings
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+# ]
+
+# # REST Framework Settings
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ),
+#     'DEFAULT_PERMISSION_CLASSES': (
+#         'rest_framework.permissions.IsAuthenticated',
+#     ),
+# }
+
+# SIMPLE_JWT = {
+#     'ACCESS_TOKEN_LIFETIME': timedelta(hours=5),
+#     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+#     'ROTATE_REFRESH_TOKENS': False,
+#     'BLACKLIST_AFTER_ROTATION': True,
+#     'AUTH_HEADER_TYPES': ('Bearer',),
+# }
+
+# AUTH_USER_MODEL = 'customers.User' 
+
+
 import os
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# Detect environment
+IS_PRODUCTION = os.environ.get('RAILWAY_STATIC_URL') or os.environ.get('RENDER') or os.environ.get('DJANGO_PRODUCTION')
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$n#o3xf@yjz*(8y1g+kw+h5yve6gip4cmp*0hm_lyo%!t7aul='
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+if IS_PRODUCTION:
+    # Production settings
+    DEBUG = False
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'production-secret-key-change-this')  # Railway will provide
+    ALLOWED_HOSTS = ['*']  # Allow all for now, change to your domain later
+    
+    # Security settings for production
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    
+    # CORS for production
+    CORS_ALLOWED_ORIGINS = [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ]
+    CORS_ALLOW_CREDENTIALS = True
+    
+else:
+    # Development settings
+    DEBUG = True
+    SECRET_KEY = 'django-insecure-$n#o3xf@yjz*(8y1g+kw+h5yve6gip4cmp*0hm_lyo%!t7aul='  # Keep your dev key
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+    
+    # CORS for development
+    CORS_ALLOWED_ORIGINS = [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ]
+    CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
-
 INSTALLED_APPS = [
    'django.contrib.admin',
     'django.contrib.auth',
@@ -29,7 +228,7 @@ INSTALLED_APPS = [
     
     # Third party
     'rest_framework',
-    'rest_framework_simplejwt',  # ADD THIS
+    'rest_framework_simplejwt',
     'corsheaders',
     
     # Local apps
@@ -38,6 +237,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ADD THIS - for static files
     'corsheaders.middleware.CorsMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -66,48 +266,42 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'crm_backend.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('POSTGRES_DB', 'crm_db'),
-#         'USER': os.environ.get('POSTGRES_USER', 'crm_user'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'crm_password'),
-#         'HOST': os.environ.get('DATABASE_HOST', 'db'),
-#         'PORT': os.environ.get('DATABASE_PORT', '5432'),
-#     }
-# }
-
-from decouple import config
-
 # Database configuration
-DATABASE_ENGINE = config('DATABASE_ENGINE', default='sqlite')
-
-if DATABASE_ENGINE == 'postgresql':
+if IS_PRODUCTION:
+    # Production database (Railway provides DATABASE_URL)
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('POSTGRES_DB', default='crm_db'),
-            'USER': config('POSTGRES_USER', default='crm_user'),
-            'PASSWORD': config('POSTGRES_PASSWORD', default='crm_password'),
-            'HOST': config('DATABASE_HOST', default='127.0.0.1'),
-            'PORT': config('DATABASE_PORT', default='5432'),
-        }
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
 else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+    # Development database
+    from decouple import config
+    
+    DATABASE_ENGINE = config('DATABASE_ENGINE', default='sqlite')
+    
+    if DATABASE_ENGINE == 'postgresql':
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': config('POSTGRES_DB', default='crm_db'),
+                'USER': config('POSTGRES_USER', default='crm_user'),
+                'PASSWORD': config('POSTGRES_PASSWORD', default='crm_password'),
+                'HOST': config('DATABASE_HOST', default='127.0.0.1'),
+                'PORT': config('DATABASE_PORT', default='5432'),
+            }
         }
-    }
+    else:
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
 
 # Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -123,29 +317,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
+# Static files
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # ADD THIS for production
 
-# CORS Settings
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+# Whitenoise static files configuration
+if IS_PRODUCTION:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+else:
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # REST Framework Settings
 REST_FRAMEWORK = {
@@ -165,4 +351,7 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
-AUTH_USER_MODEL = 'customers.User' 
+AUTH_USER_MODEL = 'customers.User'
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
